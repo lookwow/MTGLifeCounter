2600eeac9f962964b93a4d9040826f7feb5ca74b
lookwow <lookwow_@hotmail.com>
2017-01-25 15:58:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/andy/mtglifecounter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.andy.mtglifecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.baselayout);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
